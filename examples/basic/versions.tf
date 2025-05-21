ec05fa11d753dd20c899122f7a282e02d8894015
Attila Sovak <attila.sovak@gmail.com>
2025-05-21 16:04:27 +0200

    feat(prometheus): add init files

--- examples/basic/versions.tf
+++ examples/basic/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.35.0"
+      version = "~> 5"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -13,10 +13,6 @@ terraform {
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.6.0"
-    }
-    utils = {
-      source  = "cloudposse/utils"
-      version = ">= 0.17.0"
     }
   }
 }
